e461dcb1c496bc5fd86ffb1c8cc94ec52b0a52d8
Yann Braga <yannbf@gmail.com>
2019-11-10 18:39:25 +0100

    refactor(debug): use single regex for mobile devices

--- src/components/DebugInfo.vue
+++ src/components/DebugInfo.vue
@@ -23,15 +23,7 @@ export default class DebugInfo extends Vue {
   }
 
   private isMobile() {
-    return (
-      navigator.userAgent.match(/Android/i) ||
-      navigator.userAgent.match(/webOS/i) ||
-      navigator.userAgent.match(/iPhone/i) ||
-      navigator.userAgent.match(/iPad/i) ||
-      navigator.userAgent.match(/iPod/i) ||
-      navigator.userAgent.match(/BlackBerry/i) ||
-      navigator.userAgent.match(/Windows Phone/i)
-    );
+    return navigator.userAgent.match(/Android|webOS|iPhone|iPad|iPod|BlackBerry|Windows Phone/i);
   }
 }
 </script>
